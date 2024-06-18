0ef47cbdd12b6af6b19c748e29383fa65ac4315e
nurturenature <jeff.suttor@earthlink.net>
2024-06-18 11:35:58 -0700

    set Docker registry env var

--- docker/docker-compose-up.sh
+++ docker/docker-compose-up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# export JEPSEN_REGISTRY="ghcr.io/nurturenature/jepsen-docker/"
+export JEPSEN_REGISTRY="ghcr.io/nurturenature/jepsen-docker/"
 
 docker compose \
        -f jepsen-compose.yaml \
