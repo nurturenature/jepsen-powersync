3b8721b9f1b950db8ed10fd0402e9b986de5e2d7
nurturenature <jeff.suttor@earthlink.net>
2025-02-15 14:18:16 -0800

    create GitHub action for powersync_fuzz environment

--- docker/powersync-fuzz-run.sh
+++ docker/powersync-fuzz-run.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 set -e
 
-echo
-echo    "Opening a shell to the powersync-fuzz-node:"
-echo -e "\t./powersync_fuzz -h"
-echo -e "\texit to exit"
-echo
-
 docker exec \
-       -i \
        -t \
        -w /powersync_endpoint \
        powersync-fuzz-node \
-       bash
+       bash -c "$* 2>&1 | tee powersync_fuzz.log"
