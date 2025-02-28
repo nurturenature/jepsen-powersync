c9f300c67230ba1dead5b34ecf7c6efc30b5f4dc
nurturenature <jeff.suttor@earthlink.net>
2025-02-27 21:27:07 -0800

    CausalChecker.checkOp() will now give a reason for the expected value(s) in error log messages

--- powersync_endpoint/lib/causal_checker.dart
+++ powersync_endpoint/lib/causal_checker.dart
@@ -9,22 +9,32 @@ import 'log.dart';
 // e.g. multiple independent mutations would be a problem
 final _lock = Lock();
 
+// reasons why a clientState[key] value is what it is
+enum _Reasons {
+  initialValue,
+  myPreviousRead,
+  myPreviousWrite,
+  writesFollowsRead,
+}
+
 /// Check a key/value, Max Write Wins, database
 class CausalChecker {
   late final int _numClients;
   late final int _numKeys;
-  late Map<int, List<int>> _clientStates; // map of  fixed lists
-  late Map<(int, int), List<int>> _mwWfrStates; // map of fixed lists
+  final Map<int, List<int>> _clientStates = {};
+  final Map<int, List<_Reasons>> _clientReasons = {};
+  final Map<(int, int), List<int>> _mwWfrStates = {};
 
   CausalChecker(this._numClients, this._numKeys) {
-    _clientStates = {};
     // clientNum 0 reserved for pseudo client PG
     for (var clientNum = 0; clientNum <= _numClients; clientNum++) {
       _clientStates.addEntries([
         MapEntry(clientNum, List.filled(_numKeys, -1)),
       ]);
+      _clientReasons.addEntries([
+        MapEntry(clientNum, List.filled(_numKeys, _Reasons.initialValue)),
+      ]);
     }
-    _mwWfrStates = {};
   }
 
   /// Check if op has valid reads/writes.
@@ -41,6 +51,7 @@ class CausalChecker {
       } = op;
 
       final clientState = _clientStates[clientNum]!;
+      final clientReasons = _clientReasons[clientNum]!;
 
       // ok for PostgreSQL, clientType pg, to have an error op, e.g. concurrent access
       if (clientType == 'pg' && type == 'error') {
@@ -62,16 +73,27 @@ class CausalChecker {
             // transactions are atomic and repeatable read
             //   - update client state with mw/wfr state for all reads
             //   - before checking individual reads
-            _updateClientStateWithReadMwWfrState(clientState, reads);
+            _updateClientStateWithReadMwWfrState(
+              clientState,
+              clientReasons,
+              reads,
+            );
 
             // check each read k/v
             for (final kv in reads.entries) {
-              if (!_checkSingleRead(clientState, kv.key, kv.value, op)) {
+              if (!_checkSingleRead(
+                clientState,
+                clientReasons,
+                kv.key,
+                kv.value,
+                op,
+              )) {
                 return false;
               }
 
               // update client state with current read value
               clientState[kv.key] = kv.value;
+              clientReasons[kv.key] = _Reasons.myPreviousRead;
             }
 
             break;
@@ -81,13 +103,23 @@ class CausalChecker {
 
             // check each write k/v
             for (final kv in writes.entries) {
-              if (!_checkSingleWrite(clientState, kv.key, kv.value, op)) {
+              if (!_checkSingleWrite(
+                clientState,
+                clientReasons,
+                kv.key,
+                kv.value,
+                op,
+              )) {
                 return false;
               }
             }
 
             // update state to include these writes
-            _updateClientAndMwWfrStatesWithClientWrites(clientState, writes);
+            _updateClientAndMwWfrStatesWithClientWrites(
+              clientState,
+              clientReasons,
+              writes,
+            );
 
             break;
 
@@ -105,6 +137,7 @@ class CausalChecker {
   // only checks, does not update state
   bool _checkSingleRead(
     List<int> clientState,
+    List<_Reasons> clientReasons,
     int k,
     int v,
     Map<String, dynamic> op,
@@ -120,7 +153,7 @@ class CausalChecker {
     //   - read value must be >= prev value
     if (v < clientState[k]) {
       log.severe(
-        'read of {$k: $v} is less than expected read of {$k: ${clientState[k]}} in op: $op',
+        'read of {$k: $v} is less than expected read of {$k: ${clientState[k]}}, expected because ${clientReasons[k].name}, in op: $op',
       );
       debug(k, [v, clientState[k]]);
       return false;
@@ -132,6 +165,7 @@ class CausalChecker {
   // only checks, does not update state
   bool _checkSingleWrite(
     List<int> clientState,
+    List<_Reasons> clientReasons,
     int k,
     int v,
     Map<String, dynamic> op,
@@ -149,7 +183,7 @@ class CausalChecker {
     //   - write value must be > prev value
     if (v <= clientState[k]) {
       log.severe(
-        'write of {$k: $v} is less than or equal to previous client state of {$k: ${clientState[k]}} in op: $op',
+        'write of {$k: $v} is less than or equal to previous client state of {$k: ${clientState[k]}}, previous state due to ${clientReasons[k]}, in op: $op',
       );
       debug(k, [v, clientState[k]]);
       return false;
@@ -164,6 +198,7 @@ class CausalChecker {
   // only updates, does not check state for errors
   void _updateClientStateWithReadMwWfrState(
     List<int> clientState,
+    List<_Reasons> clientReasons,
     Map<int, int> reads,
   ) {
     for (final readKv in reads.entries) {
@@ -183,6 +218,7 @@ class CausalChecker {
       for (var k = 0; k < _numKeys; k++) {
         if (mwWfrState[k] > clientState[k]) {
           clientState[k] = mwWfrState[k];
+          clientReasons[k] = _Reasons.writesFollowsRead;
         }
       }
     }
@@ -194,6 +230,7 @@ class CausalChecker {
   // only updates, does not check state for errors
   void _updateClientAndMwWfrStatesWithClientWrites(
     List<int> clientState,
+    List<_Reasons> clientReasons,
     Map<int, int> writes,
   ) {
     // transactions are atomic
@@ -201,6 +238,7 @@ class CausalChecker {
     //   - before updating mw/wfr states
     for (final writeKv in writes.entries) {
       clientState[writeKv.key] = writeKv.value;
+      clientReasons[writeKv.key] = _Reasons.myPreviousWrite;
     }
 
     // all mw/wfr for these writes have the same immutable client state
@@ -215,20 +253,20 @@ class CausalChecker {
   // log clients that have {k: v} in their state
   // log mw/wfr states that were a {k: v} write
   void debug(int k, Iterable<int> vs) {
-    log.info('CausalChecker client states:');
+    log.info('CausalChecker client states with {$k: $vs}:');
     for (var clientNum = 0; clientNum <= _numClients; clientNum++) {
       // only log client if its state for k is in vs
       final v = _clientStates[clientNum]![k];
       if (vs.contains(v)) {
         log.info(
-          '\t{$k: $v} in client $clientNum: ${_clientStates[clientNum]}',
+          '\t{$k: $v}: in client $clientNum: reason: ${_clientReasons[clientNum]![k].name}',
         );
       }
     }
 
     log.info('CausalChecker Monotonic Read/Write Follows Reads states:');
     for (final v in vs) {
-      log.info('\t{$k: $v}: ${_mwWfrStates[(k, v)]}');
+      log.info('\t{$k: $v}: written when: ${_mwWfrStates[(k, v)]}');
     }
   }
 }
