1b2a924c3b0bf5b64841ea13c171351ddc27b013
nurturenature <jeff.suttor@earthlink.net>
2025-03-01 16:06:36 -0800

    change loop shell command test to exit code > 1, stop at anything other than success or not strong convergence

--- docker/powersync-fuzz-loop.sh
+++ docker/powersync-fuzz-loop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -o pipefail
 
-# ./powersync-fuzz-loop.sh ./powersync_fuzz --table mww --clients 10 --rate 10 --time 200 --no-postgresql --disconnect --no-stop --no-kill --partition --no-pause --interval 5
+# ./powersync-fuzz-loop.sh ./powersync_fuzz --table mww --clients 10 --rate 10 --time 100 --postgresql --disconnect --no-stop --no-kill --partition --no-pause --interval 5
 
 # insure environment is built
 ./powersync-fuzz-down.sh
@@ -21,8 +21,8 @@ do
     echo "exit code from powersync-fuzz-run.sh: $ec"
 
     # desired exit code?
-    if [ $ec -eq 2 ]; then
-        echo "found suspect non-monotonic read!"
+    if [ $ec -gt 1 ]; then
+        echo "found suspect, exit code $ec > 1, non-monotonic read!"
         echo "leaving docker PowerSync custer up"
         echo "copying powersync_fuzz.log from powersync-fuzz-node container to this host"
         docker cp powersync-fuzz-node:/jepsen/jepsen-powersync/powersync_endpoint/powersync_fuzz.log .
