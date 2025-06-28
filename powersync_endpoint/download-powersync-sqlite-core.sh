1451b01706ced799e96f9985239e4b5c96874525
nurturenature <jeff.suttor@earthlink.net>
2025-06-28 16:27:43 -0700

    update deps

--- powersync_endpoint/download-powersync-sqlite-core.sh
+++ powersync_endpoint/download-powersync-sqlite-core.sh
@@ -2,4 +2,4 @@
 set -e
 
 # app is responsible for getting native lib
-wget --no-verbose -O libpowersync_x64.so https://github.com/powersync-ja/powersync-sqlite-core/releases/download/v0.4.0/libpowersync_x64.so
+wget --no-verbose -O libpowersync_x64.so https://github.com/powersync-ja/powersync-sqlite-core/releases/download/v0.4.1/libpowersync_x64.so
